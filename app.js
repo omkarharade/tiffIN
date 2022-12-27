6b046bd2703a8b0db975e9ef1754993afc5ee2cb
Omkar Harade <omkarharade1301@gmail.com>
2022-12-27 21:39:56 +0530

    updated files

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 //jshint esversion:6
 
-
 // var string = "2021-04-26T14:40:16.461Z";    <------ to display date in right format
 // var string2 = string.split('T')[0];
 
 // console.log(string.split('T')[0]);
-// var splitString = string2.split("-"); 
+// var splitString = string2.split("-");
 
-// var reverseArray = splitString.reverse(); 
+// var reverseArray = splitString.reverse();
 
-// var joinArray = reverseArray.join("/"); 
+// var joinArray = reverseArray.join("/");
 // console.log(joinArray);
 
 function convToShortDate(str) {
-
   // var string = "2021-04-26T14:40:16.461Z";    <------ to display date in right format
-  var string2 = string.split('T')[0];
+  var string2 = string.split("T")[0];
 
-  console.log(string.split('T')[0]);
+  console.log(string.split("T")[0]);
   var splitString = string2.split("-");
 
   var reverseArray = splitString.reverse();
 
   var joinArray = reverseArray.join("/");
   return joinArray;
-
 }
 
 const express = require("express");
@@ -34,9 +31,7 @@ const mongoose = require("mongoose");
 const app = express();
 const bcrypt = require("bcrypt");
 const session = require("express-session");
-const {
-  noConflict
-} = require("lodash");
+const { noConflict } = require("lodash");
 const saltRounds = 11;
 
 app.use(express.static("public"));
@@ -50,39 +45,40 @@ app.use(
 // var customPhoneNo = null;    <--- never used them
 // var customerName = null;
 
-
-mongoose.connect("mongodb://localhost:27017/tiffinDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-
-});
+mongoose.connect(
+  "mongodb+srv://admin-omkar:Omkar@1301@cluster0.ytoxl.mongodb.net/tiffinDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'NAME required']
+    required: [true, "NAME required"],
   },
   phoneNo: {
     type: String,
     minLength: 10,
     maxLength: 10,
-    required: [true, 'PHONE NUMBER required'],
-    unique: [true, 'user with the phone number already exists']
+    required: [true, "PHONE NUMBER required"],
+    unique: [true, "user with the phone number already exists"],
   },
   address: {
     type: String,
-    required: [true, 'ADDRESS required']
+    required: [true, "ADDRESS required"],
   },
   password: {
     type: String,
-    required: [true, 'PASSWORD required']
+    required: [true, "PASSWORD required"],
   },
   role: {
     type: String,
-    default: "customer"
-  }
+    default: "customer",
+  },
 });
 
 const User = mongoose.model("User", userSchema);
@@ -91,9 +87,8 @@ const itemSchema = new mongoose.Schema({
   itemId: String,
   itemName: String,
   itemPrice: Number,
-  itemAvail: Boolean
+  itemAvail: Boolean,
 });
-
 
 const Item = mongoose.model("Item", itemSchema);
 
@@ -106,8 +101,6 @@ const Item = mongoose.model("Item", itemSchema);
 
 // item.save();
 
-
-
 function date() {
   var date = new Date();
   // date.setDate(date.getDate() + no. of days);
@@ -115,7 +108,7 @@ function date() {
   date.setMinutes(date.getMinutes() + 30);
   var isodate = date.toISOString();
 
-  var string2 = isodate.split('T')[0];
+  var string2 = isodate.split("T")[0];
 
   var splitString = string2.split("-");
 
@@ -124,39 +117,33 @@ function date() {
   return reverseArray.join("/");
 }
 
-
 const orderSchema = new mongoose.Schema({
   orderId: mongoose.Schema.Types.ObjectId,
   customerId: String,
   orderDate: {
     type: String,
-    default: date()
+    default: date(),
   },
   orderList: itemSchema,
   payment: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 
 const Order = mongoose.model("Order", orderSchema);
 
-// Order.deleteOne({_id : 5f9eab7caa0a5a0ad821b70c"}, function(err){        <------ just in case to delete wrong orders 
+// Order.deleteOne({_id : 5f9eab7caa0a5a0ad821b70c"}, function(err){        <------ just in case to delete wrong orders
 //   if(err)
 //      console.log(err);
 //      else
 //      console.log("Successfully deleted one item");
 // });
 
-
-
-
 app.get("/", function (req, res) {
   // res.render("categories");
   res.render("landing-page");
 });
-
-
 
 app.get("/signup", function (req, res) {
   res.render("signup");
@@ -171,78 +158,74 @@ app.get("/error", function (req, res) {
 });
 
 app.post("/change-avail-admin", async function (req, res) {
-
   const ItemId = req.body.clickedItemId;
 
-  console.log(typeof (req.body.clickedItemId));
-  console.log(typeof (req.body.itemStatus));
+  console.log(typeof req.body.clickedItemId);
+  console.log(typeof req.body.itemStatus);
   console.log("itemid : " + ItemId);
   var changeItemStatus;
   if (req.body.itemStatus === "true") {
     changeItemStatus = false;
   } else {
-    changeItemStatus = true
+    changeItemStatus = true;
   }
 
-
-  const foundItem = await Item.updateOne({
-    itemId: req.body.clickedItemId
-  }, {
-    itemAvail: changeItemStatus
-  });
-
+  const foundItem = await Item.updateOne(
+    {
+      itemId: req.body.clickedItemId,
+    },
+    {
+      itemAvail: changeItemStatus,
+    }
+  );
 
   res.send("changed status");
 });
 
-
-
 app.post("/admin-view", async function (req, res) {
-
   var customers;
   var ordersArray = [];
 
-  await User.find({
-    role: "customer"
-  }, function (err, foundUsers) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(foundUsers)
-      customers = foundUsers;
-    }
-  });
-
-  for (var i = 0; i < customers.length; i++) {
-
-    await Order.find({
-      customerId: customers[i].phoneNo
-    }, function (err, foundOrders) {
-
+  await User.find(
+    {
+      role: "customer",
+    },
+    function (err, foundUsers) {
       if (err) {
         console.log(err);
       } else {
-
-        ordersArray.push(foundOrders);
+        console.log(foundUsers);
+        customers = foundUsers;
       }
-    });
+    }
+  );
 
+  for (var i = 0; i < customers.length; i++) {
+    await Order.find(
+      {
+        customerId: customers[i].phoneNo,
+      },
+      function (err, foundOrders) {
+        if (err) {
+          console.log(err);
+        } else {
+          ordersArray.push(foundOrders);
+        }
+      }
+    );
   }
 
   res.render("orders-details", {
     foundOrders: ordersArray,
     customers: customers,
-    customerCount: customers.length
+    customerCount: customers.length,
   });
-
-
 });
 
 app.post("/custom-date-orders-details", async function (req, res) {
-
   var dd = req.body.dd;
   var mm = req.body.mm;
-  var yyyy = req.body.yyyy
+  var yyyy = req.body.yyyy;
 
   if (dd.length == 1) {
     dd = "0" + dd;
@@ -253,50 +236,49 @@ app.post("/custom-date-orders-details", async function (req, res) {
   }
 
   if (yyyy.length == 4) {
-    const ordersDate = dd + "/" + mm + "/" + yyyy
+    const ordersDate = dd + "/" + mm + "/" + yyyy;
     var customers;
     var ordersArray = [];
 
-    await User.find({
-      role: "customer"
-    }, function (err, foundUsers) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(foundUsers)
-        customers = foundUsers;
-      }
-    });
-
-    for (var i = 0; i < customers.length; i++) {
-
-      await Order.find({
-        customerId: customers[i].phoneNo,
-        orderDate: ordersDate
-      }, function (err, foundOrders) {
-
+    await User.find(
+      {
+        role: "customer",
+      },
+      function (err, foundUsers) {
         if (err) {
           console.log(err);
         } else {
-
-          ordersArray.push(foundOrders);
+          console.log(foundUsers);
+          customers = foundUsers;
         }
-      });
+      }
+    );
 
+    for (var i = 0; i < customers.length; i++) {
+      await Order.find(
+        {
+          customerId: customers[i].phoneNo,
+          orderDate: ordersDate,
+        },
+        function (err, foundOrders) {
+          if (err) {
+            console.log(err);
+          } else {
+            ordersArray.push(foundOrders);
+          }
+        }
+      );
     }
 
     res.render("orders-details", {
       foundOrders: ordersArray,
       customers: customers,
-      customerCount: customers.length
+      customerCount: customers.length,
     });
   } else {
     res.send("Enter correct date format");
   }
-
 });
-
-
 
 app.post("/server", async function (req, res) {
   const resArray = req.body.myArray;
@@ -308,42 +290,39 @@ app.post("/server", async function (req, res) {
     console.log(resArray);
 
     for (var i = 0; i < resArray.length; i++) {
-
-      await Item.findOne({
-        itemId: {
-          $eq: resArray[i]
+      await Item.findOne(
+        {
+          itemId: {
+            $eq: resArray[i],
+          },
+        },
+        function (err, foundItem) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(foundItem);
+            console.log("itemFound");
+            const order = new Order({
+              customerId: phoneNo,
+              orderList: foundItem,
+            });
+            order.save();
+          }
         }
-      }, function (err, foundItem) {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log(foundItem);
-          console.log("itemFound");
-          const order = new Order({
-            customerId: phoneNo,
-            orderList: foundItem
-          });
-          order.save();
-        }
-      });
-
-
+      );
     }
-
 
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    // error code starts   
+    // error code starts
 
-
-    // this code is having a problem of save() being asynchronous see afterwards 
-
+    // this code is having a problem of save() being asynchronous see afterwards
 
     // const order = new Order({
     //   customerId : phoneNo
-    // });       
+    // });
 
-    // for(var i = 0 ; i < resArray.length; i++ ){            
+    // for(var i = 0 ; i < resArray.length; i++ ){
 
     //   await Item.findOne({itemId : {$eq :resArray[i]} },  function (err, foundItem) {
     //     if (err){
@@ -359,7 +338,6 @@ app.post("/server", async function (req, res) {
     //     }
     //  });
 
-
     // }
     //  order.save( function(err, orderData){
     //   if (err){
@@ -369,13 +347,10 @@ app.post("/server", async function (req, res) {
     //     console.log(orderData);
     //     console.log("this is the data which is having error");
     //   }
-    // });       
+    // });
 
-    //  check later code ends 
+    //  check later code ends
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
 
     // const orderSchema = new mongoose.Schema({
     //   orderId: mongoose.Schema.Types.ObjectId,
@@ -383,7 +358,6 @@ app.post("/server", async function (req, res) {
     //   orderDate: { type: Date, default: Date.now },
     //   orderList: [itemSchema],
     // });
-
 
     // Order.insertMany(orders , function(err){
     //   if(err)
@@ -394,19 +368,17 @@ app.post("/server", async function (req, res) {
 
     console.log("status -> success");
     res.send({
-      "status": "successfull"
+      status: "successfull",
     });
   } else {
     console.log("status -> fail");
     res.send({
-      "status": "fail"
+      status: "fail",
     });
   }
 });
 
 app.post("/proceed", function (req, res) {
-
-
   console.log("I am inside /proceed");
 
   const customerName = req.body.fname;
@@ -428,23 +400,23 @@ app.post("/proceed", function (req, res) {
 
   //       }
   // }).then(res.render("success", {cusName: req.body.fname, orders: ordersArray, orderDate : currentDate}));
-  Order.find({
-    customerId: customerId,
-    orderDate: currentDate
-  }, function (err, foundOrders) {
-    if (err) {
-      console.log(err);
-    } else {
-
-      console.log(foundOrders);
-      res.render("success", {
-        orders: foundOrders,
-        orderDate: currentDate,
-
-      });
-
+  Order.find(
+    {
+      customerId: customerId,
+      orderDate: currentDate,
+    },
+    function (err, foundOrders) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(foundOrders);
+        res.render("success", {
+          orders: foundOrders,
+          orderDate: currentDate,
+        });
+      }
     }
-  });
+  );
 
   //  function getOrdersPromise(customerId, currentDate){
   //   var promise =  Order.find(
@@ -470,21 +442,17 @@ app.post("/proceed", function (req, res) {
   //       res.render("success", {cusName: req.body.fname, orders: orderDetails});
   //      }
   //    });
-
 });
 app.get("/itemAvail", async function (req, res) {
-  var availArray = []
+  var availArray = [];
   await Item.find({}, function (err, foundItems) {
-
     if (err) {
       console.log(err);
     } else {
-
       foundItems.forEach(function (item) {
         availArray.push(item.itemAvail);
       });
     }
-
   });
   console.log("this is the available items array");
   console.log(availArray);
@@ -492,24 +460,24 @@ app.get("/itemAvail", async function (req, res) {
 });
 
 app.post("/delete", function (req, res) {
-
   const orderId = req.body.checkbox;
   const customerId = req.body.customerId;
 
-  Order.findByIdAndDelete({
-    _id: orderId
-  }, function (err, docs) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log("Deleted : ", docs);
+  Order.findByIdAndDelete(
+    {
+      _id: orderId,
+    },
+    function (err, docs) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Deleted : ", docs);
+      }
     }
-  });
+  );
 
   res.redirect("/" + customerId);
-
 });
-
 
 app.post("/my-orders", function (req, res) {
   const customerId = req.body.customerId;
@@ -517,29 +485,27 @@ app.post("/my-orders", function (req, res) {
   res.redirect("/" + customerId);
 });
 
-
 app.get("/:customerId", function (req, res) {
-
   const customerId = req.params.customerId;
   const currentDate = date();
 
-  Order.find({
-    customerId: customerId,
-    orderDate: currentDate
-  }, function (err, foundOrders) {
-    if (err) {
-      console.log(err);
-    } else {
-
-      console.log(foundOrders);
-      res.render("success", {
-        orders: foundOrders,
-        orderDate: currentDate
-      });
-
+  Order.find(
+    {
+      customerId: customerId,
+      orderDate: currentDate,
+    },
+    function (err, foundOrders) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(foundOrders);
+        res.render("success", {
+          orders: foundOrders,
+          orderDate: currentDate,
+        });
+      }
     }
-  });
-
+  );
 });
 
 app.post("/set-item-avail", async function (req, res) {
@@ -553,7 +519,7 @@ app.post("/set-item-avail", async function (req, res) {
     }
   });
   res.render("item-avail", {
-    items: items
+    items: items,
   });
 });
 
@@ -568,16 +534,13 @@ app.post("/signup", function (req, res) {
           password: hash,
         });
         register.save(function (err) {
-
           if (err) {
             res.redirect("/error");
-
           } else {
             console.log("successfully registered");
             res.redirect("/login");
           }
         });
-
       } else {
         console.log("error in passwords");
         res.redirect("/error");
@@ -587,11 +550,11 @@ app.post("/signup", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
-
   const username = req.body.myPhoneNo;
   const password = req.body.password;
 
-  User.findOne({
+  User.findOne(
+    {
       phoneNo: username,
     },
     function (err, foundUser) {
@@ -611,14 +574,12 @@ app.post("/login", function (req, res) {
                   titleName: foundUser.name,
                   userPhoneNo: foundUser.phoneNo,
                 });
-
               } else {
                 res.render("admin-page", {
                   adminName: foundUser.name,
-                  adminPhoneNo: foundUser.phoneNo
+                  adminPhoneNo: foundUser.phoneNo,
                 });
               }
-
             } else {
               console.log("fail");
               res.render("error");
@@ -628,9 +589,7 @@ app.post("/login", function (req, res) {
       }
     }
   );
-
 });
-
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server started on port 3000");
